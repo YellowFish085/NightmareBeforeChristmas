12feb72972dfef57d536f26fe2cdbf78faa3cc9f
Yellow Fish <theo.costantini@gmail.com>
2015-12-10 20:13:30 +0100

    Ajout méthode loadScene

--- GLImac-Template/glimac/include/glimac/fileloader.hpp
+++ GLImac-Template/glimac/include/glimac/fileloader.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <stdlib.h>
+#include <iostream>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 namespace Projet
 {
-
+	const aiScene* loadScene( const char* filename );
 }
