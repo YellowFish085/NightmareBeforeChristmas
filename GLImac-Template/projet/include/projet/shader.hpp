6c21a1a26734900d3993cef443ea6e1e970f2d7e
Yellow Fish <theo.costantini@gmail.com>
2015-12-14 23:33:19 +0100

    Ajout de la classe ShaderProgram

---
+++ GLImac-Template/projet/include/projet/shader.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <string.h>
+
+#include <glimac/FilePath.hpp>
+#include <glimac/Program.hpp>
+
+namespace Projet
+{
+	class ShaderProgram
+	{
+	public:
+		ShaderProgram(const glimac::FilePath applicationPath);
+		~ShaderProgram();
+
+		bool init(const char* vsFilename, const char* fsFilename);
+		void use();
+
+	private:
+		glimac::FilePath _applicationPath;	// Pour le chemin de chargement des shaders
+		glimac::Program _Program;						// Programme glimac
+		GLint _MVPlocation;									// Model View Projetction matrix location
+		GLint _MVlocation;									// Model View matrix location
+		GLint _Nlocation;										// Normal matrix location
+	};
+}
