016af980fa861dfcb73add2e492d78c64d9e0f83
Yellow Fish <theo.costantini@gmail.com>
2015-12-15 14:20:59 +0100

    Modification du main.cpp pour correspondre à l'actuelle version de l'app

--- GLImac-Template/dev_default_assimp/main.cpp
+++ GLImac-Template/dev_default_assimp/main.cpp
@@ -1,49 +1,29 @@
-#include <glimac/SDLWindowManager.hpp>
-#include <GL/glew.h>
 #include <iostream>
 
-#include <projet/scene.hpp>
+#include <projet/engine.hpp>
 
 using namespace glimac;
 
 int main(int argc, char** argv) {
-    Projet::Scene* scene = new Projet::Scene( "k" );
-    
-    // Initialize SDL and open a window
-    SDLWindowManager windowManager(800, 600, "GLImac");
-
-    // Initialize glew for OpenGL3+ support
-    GLenum glewInitError = glewInit();
-    if(GLEW_OK != glewInitError) {
-        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+    Projet::AppEngine* eng = new Projet::AppEngine(argv[0]);
+    if (!eng->init("shader.vs.glsl", "shader.fs.glsl")) {
         return EXIT_FAILURE;
     }
 
-    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+    // // Application loop:
+    // bool done = false;
+    // while(!done) {
+    //     // Event loop:
+    //     SDL_Event e;
+    //     while(windowManager.pollEvent(e)) {
+    //         if(e.type == SDL_QUIT) {
+    //             done = true; // Leave the loop after this iteration
+    //         }
+    //     }
 
-    /*********************************
-     * HERE SHOULD COME THE INITIALIZATION CODE
-     *********************************/
-
-    // Application loop:
-    bool done = false;
-    while(!done) {
-        // Event loop:
-        SDL_Event e;
-        while(windowManager.pollEvent(e)) {
-            if(e.type == SDL_QUIT) {
-                done = true; // Leave the loop after this iteration
-            }
-        }
-
-        /*********************************
-         * HERE SHOULD COME THE RENDERING CODE
-         *********************************/
-
-        // Update the display
-        windowManager.swapBuffers();
-    }
+    //     // Update the display
+    //     windowManager.swapBuffers();
+    // }
 
     return EXIT_SUCCESS;
 }
