c1c7467478812c01d13dfe05b0153d93741d1987
Yellow Fish <theo.costantini@gmail.com>
2015-12-15 21:34:21 +0100

    MAJ engine pour afficher un mesh

--- GLImac-Template/projet/include/projet/engine.hpp
+++ GLImac-Template/projet/include/projet/engine.hpp
@@ -7,23 +7,30 @@
 #include <glimac/FilePath.hpp>
 
 #include "shader.hpp"
+#include "Mesh.hpp" // Sans doute à retirer une fois que l'objet scene sera fait
 
 namespace Projet
 {
+	/**
+	 * Class AppEngine
+	 * Create a global class that will handle the application
+	 **/
 	class AppEngine
 	{
 	public:
 		AppEngine(const char* appPath);
 		~AppEngine();
 
+		// Initialize the components of the app
 		bool init(const char* vsFilename, const char* fsFilename);
 		void run();
 
 	private:
-		bool initSdlOpenGl();
-		void renderScene();
+		bool initSdlOpenGl();		// Initalize SDL and OpenGl
+		void renderScene();			
 		void keyboardEvents();
 		void mouseEvents();
+
 		glimac::FilePath* _ApplicationPath;
 		glimac::SDLWindowManager* _WindowManager;
 		ShaderProgram* _Program;
--- GLImac-Template/projet/src/engine.cpp
+++ GLImac-Template/projet/src/engine.cpp
@@ -15,15 +15,45 @@ namespace Projet
 
 	bool AppEngine::init(const char* vsFilename, const char* fsFilename)
 	{
+		// Initialize SDL and Opengl
+		// Exit the app if something went wrong
 		if (!initSdlOpenGl()) {
 			std::cerr << "Erreur lors de l'initialisation de SDL et OpenGL." << std::endl;
 			return false;
 		}
 
+		// Initialize the shader program
+		// Exit the app if something went wrong
 		_Program = new ShaderProgram(_ApplicationPath);
 		if (!_Program->init(vsFilename, fsFilename)) {
 			std::cerr << "Erreur lors de l'initialisation du ShaderProgram." << std::endl;
 			return false;
+		}
+
+		// A RETIRER UNE FOIS L'OBJET SCENE FINI
+		Mesh* mesh = new Mesh(_ApplicationPath);
+		if (mesh->loadMesh("Content/box.obj")) {
+			std::cout << "Mesh - initalisée correctement." << std::endl;
+
+			glm::mat4 pMatrix, wMatrix, nMatrix;
+
+			pMatrix  = glm::perspective(glm::radians(70.f), 800.f/600.f, 0.1f, 100.f);
+			wMatrix = glm::translate(wMatrix, glm::vec3(0, 0, -5));
+			nMatrix  = glm::transpose(glm::inverse(wMatrix));
+
+			_Program->setTextureLocation(0);
+			_Program->setProjectionMatrix( glm::value_ptr(pMatrix * wMatrix) );
+			_Program->setWorldMatrix( glm::value_ptr(wMatrix) );
+			_Program->setNormalMatrix( glm::value_ptr(wMatrix) );
+
+			mesh->render();
+			_WindowManager->swapBuffers();
+
+			getchar(); // juste la pour stoper l'app
+			// FIN A RETIRER
+		}
+		else {
+			std::cerr << "Mesh pas ok :c" << std::endl;
 		}
 
 		return true;
@@ -50,7 +80,7 @@ namespace Projet
 
 		std::cout << "SDL et OpenGl - initialisés correctement." << std::endl;
 		std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-		std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+		std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl << std::endl;
 
 		return true;
 	}
