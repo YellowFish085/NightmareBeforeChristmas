d862b9571e2ce1b30a02184591da269cf044cf39
Christophe AC <assante.christophe@laposte.net>
2015-12-15 11:51:23 +0100

    VERTEX - Optimisation des constructeurs
    utilisation de listes d'initalisation

--- GLImac-Template/projet/src/Vertex.cpp
+++ GLImac-Template/projet/src/Vertex.cpp
@@ -3,27 +3,34 @@
 /* CONSTRUCTORS */
 /* |_Default */
 Vertex::Vertex() :
-  _position(glm::vec3(0)), _textcoord(glm::vec2(0)), _normal(glm::vec3(0)){
+  _position(glm::vec3(0)),
+  _textcoord(glm::vec2(0)),
+  _normal(glm::vec3(0)){
 
 };
 
 /* |_Others */
 Vertex::Vertex(glm::vec3 position, glm::vec2 textcoord, glm::vec3 normal) :
-  _position(position), _textcoord(textcoord), _normal(normal){
+  _position(position),
+  _textcoord(textcoord),
+  _normal(normal){
 
 };
 
 Vertex::Vertex(float posX, float posY, float posZ, float tcX, float tcY, float normX, float normY, float normZ) :
-  _position(glm::vec3(posX, posY, posZ)), _textcoord(glm::vec2(tcX, tcY)), _normal(glm::vec3(normX, normY, normZ)){
+  _position(glm::vec3(posX, posY, posZ)),
+  _textcoord(glm::vec2(tcX, tcY)),
+  _normal(glm::vec3(normX, normY, normZ)){
 
 };
 
 /* |_Copy */
-Vertex::Vertex(const Vertex& vertexToCopy) {
-  _position = vertexToCopy._position;
-  _textcoord = vertexToCopy._textcoord;
-  _normal = vertexToCopy._normal;
-}
+Vertex::Vertex(const Vertex& vertexToCopy) :
+  _position(vertexToCopy._position),
+  _textcoord(vertexToCopy._textcoord),
+  _normal(vertexToCopy._normal){
+
+};
 
 /* OVERLOADING */
 /* Operators */
