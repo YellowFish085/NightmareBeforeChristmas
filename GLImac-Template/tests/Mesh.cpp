7d8cbbfaf10534af4b031e360a866613a510b6c5
Christophe AC <assante.christophe@laposte.net>
2015-12-15 12:01:19 +0100

    MESH - ajout de test pour copie, nbVertices, nbIndices, nbFaces

--- GLImac-Template/tests/Mesh.cpp
+++ GLImac-Template/tests/Mesh.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "projet/Mesh.hpp"
 
-TEST(Mesh, init_setters){
+TEST(Mesh, init_and_setters){
   Mesh m1;
   ASSERT_FALSE(m1.getVertices());
   ASSERT_FALSE(m1.getIndices());
@@ -28,6 +28,22 @@ TEST(Mesh, init_setters){
   Mesh m2(v, i, 0);
 
   EXPECT_EQ(m1, m2);
+}
+
+TEST(Mesh, copy_constructor){
+  std::vector<Vertex> v;
+  v.push_back(Vertex(glm::vec3(1,1,1),
+                      glm::vec2(1,1),
+                      glm::vec3(1,1,1)));
+
+  std::vector<uint32_t> i;
+  i.push_back(1);
+
+  Mesh m1(v, i, 0);
+
+  Mesh m2 = m1;
+
+  EXPECT_EQ(m1,m2);
 }
 
 TEST(Mesh, push_back) {
@@ -58,4 +74,44 @@ TEST(Mesh, getters) {
 
   EXPECT_EQ(*(m.getVertices()), *(v.data()));
   EXPECT_EQ(*(m.getIndices()), *(i.data()));
+}
+
+TEST(Mesh, nbAttributes) {
+  Mesh m1;
+  Vertex v(glm::vec3(1,1,1),
+          glm::vec2(1,1),
+          glm::vec3(1,1,1));
+
+  EXPECT_EQ(m1.nbVertices(), 0);
+
+  m1.addVertex(v);
+
+  EXPECT_EQ(m1.nbVertices(), 1);
+
+  m1.addVertex(v);
+
+  EXPECT_EQ(m1.nbVertices(), 2);
+
+  m1.addVertex(v);
+
+  EXPECT_EQ(m1.nbVertices(), 3);
+
+
+  EXPECT_EQ(m1.nbIndices(), 0);
+  EXPECT_EQ(m1.nbFaces(), 0);
+
+  m1.addIndex(1);
+
+  EXPECT_EQ(m1.nbIndices(), 1);
+  EXPECT_EQ(m1.nbFaces(), 0);
+
+  m1.addIndex(1);
+
+  EXPECT_EQ(m1.nbIndices(), 2);
+  EXPECT_EQ(m1.nbFaces(), 0);
+
+  m1.addIndex(1);
+
+  EXPECT_EQ(m1.nbIndices(), 3);
+  EXPECT_EQ(m1.nbFaces(), 1);
 }
