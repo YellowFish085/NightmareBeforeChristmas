d86707aaf716d23ce780c1c5e14b6b3a5a41a1c6
Yellow Fish <theo.costantini@gmail.com>
2015-12-17 23:01:52 +0100

    [Clean] Nettoyage + ajout de commentaires de engine.hpp

--- GLImac-Template/projet/include/projet/engine.hpp
+++ GLImac-Template/projet/include/projet/engine.hpp
@@ -5,10 +5,10 @@
 #include <iostream>
 #include <GL/glew.h>
 
+#include <json/json.h>
+
 #include <glimac/SDLWindowManager.hpp>
 #include <glimac/FilePath.hpp>
-
-#include <json/json.h>
 
 #include "shader.hpp"
 #include "scene.hpp"
@@ -26,16 +26,15 @@ namespace Projet
 		AppEngine(const char* appPath);
 		~AppEngine();
 
-		// Initialize the components of the app
-		bool init(const char* vsFilename, const char* fsFilename);
-		void run();
+		bool init(const char* vsFilename, const char* fsFilename);	// Initialize the components of the app
+		void run();																									// Render loop
 
 	private:
-		bool initSdlOpenGl();		// Initalize SDL and OpenGl
-		bool initScenes(const char* filename);
-		void renderScene();
-		bool keyboardEvents();
-		void mouseEvents();
+		bool initSdlOpenGl();										// Initalize SDL and OpenGl
+		bool initScenes(const char* filename);	// Initialize the scenes
+		void renderScene();											// Render the current scene
+		bool keyboardEvents();									// Keyboard event (return value for alt+f4)
+		void mouseEvents();											// Mouse events
 
 		glimac::FilePath* _ApplicationPath;
 		glimac::SDLWindowManager* _WindowManager;
