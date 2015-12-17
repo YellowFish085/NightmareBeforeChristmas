d495ab685c19d14a14e7557a72505b60ffcb865c
Yellow Fish <theo.costantini@gmail.com>
2015-12-17 23:39:39 +0100

    [Clean] Nettoyage + commentaires de scene.cpp

--- GLImac-Template/projet/src/scene.cpp
+++ GLImac-Template/projet/src/scene.cpp
@@ -2,61 +2,57 @@
 
 namespace Projet
 {
-
-	/* CONSTRUCTORS */
-	/* Default */
 	Scene::Scene(const glimac::FilePath* applicationPath, ShaderProgram* program):
-		_applicationPath(applicationPath),
-		_program(program),
-		_camera(TrackballCamera())
+		_ApplicationPath(applicationPath),
+		_Program(program),
+		_Camera(TrackballCamera())
 	{};
 
-	/* DESTRUCTOR */
 	Scene::~Scene() {
-		for (auto mesh = _meshes.begin(); mesh != _meshes.end(); ++mesh) {
+		for (auto mesh = _Meshes.begin(); mesh != _Meshes.end(); ++mesh) {
 			delete(*mesh);
 		}
 	}
 
-	/* INIT */
 	bool Scene::init(const char* sceneFilePath)
 	{
-		std::ifstream sceneFile((_applicationPath->dirPath() + "/../assets/" + sceneFilePath).c_str(), std::ios::in);
+		// Load the json with the scene datas
+		std::ifstream sceneFile((_ApplicationPath->dirPath() + "/../assets/" + sceneFilePath).c_str(), std::ios::in);
 
 		if (!sceneFile) {
-			std::cerr << "Impossible d'ouvrir le fichier de scene  à l'adresse : " << (_applicationPath->dirPath() + "/../assets/" + sceneFilePath).c_str() << std::endl;
+			std::cerr << "- Impossible d'ouvrir le fichier de scene  à l'adresse : " << (_ApplicationPath->dirPath() + "/../assets/" + sceneFilePath).c_str() << std::endl;
 			return false;
 		}
-		else {
-			std::cout << "Fichier de scene ouvert" << std::endl;
-			std::cout << sceneFile << std::endl;
-		}
+		std::cout << "- Fichier " << sceneFilePath << " ouvert..." << std::endl;
 
+		// Retrieve the root node
 		Json::Value root;
 		Json::Reader reader;
 
 		if (!reader.parse(sceneFile, root, false)) {
-			// report to the user the failure and their locations in the document.
-      std::cerr  << "Erreur lors de la récupération du json !" << std::endl;
+      std::cerr  << "- Erreur lors de la récupération du json !" << std::endl;
 			return false;
 		}
 
-		std::cout << "type: " << root.type() << std::endl;
-
-		const Json::Value meshes = root["meshes"]; // Get the meshes node
+		// Retrieve the "meshes" node
+		const Json::Value meshes = root["meshes"];
 		if (meshes == 0) {
-			std::cerr << "Impossible de récupérer le noeud meshes !" << std::endl;
+			std::cerr << "- Impossible de récupérer le noeud meshes !" << std::endl;
 			return false;
 		}
 
+		// Initialize each mesh
 		for (int i = 0; i < meshes.size(); i++) {
-			std::cout << i << std::endl;
-			Mesh* tmpMesh = new Mesh(_applicationPath);
+			std::cout << "- Mesh " << i+1 << " sur " << meshes.size() << " - initialisation..." << std::endl;
 
+			Mesh* tmpMesh = new Mesh(_ApplicationPath);
+
+			// Initialize the mesh
 			if (tmpMesh->loadMesh(meshes[i].get("source", 0).asString().c_str())) {
-				std::cout << "Mesh - initalisée correctement." << std::endl;
+				std::cout << "- Mesh " << i+1 << " initalisée correctement." << std::endl;
 			}
 
+			// Set the relative coords, rotation and scale of the mesh
 			tmpMesh->setPosition(glm::vec3(
 				meshes[i].get("position",0).get("x",0).asFloat(),
 				meshes[i].get("position",0).get("y",0).asFloat(),
@@ -76,17 +72,19 @@ namespace Projet
 				meshes[i].get("scale",0).get("z",1).asFloat()
 			));
 
-			_meshes.push_back(tmpMesh);
+			_Meshes.push_back(tmpMesh);
 
 		}
 
-		const Json::Value cameraDatas = root["camera"]; // Get the camera node
+		// Retrieve the camera node, used to set the défault caméra
+		const Json::Value cameraDatas = root["camera"];
 		if (cameraDatas == 0) {
-			std::cerr << "Impossible de récupérer le noeud camera !" << std::endl;
+			std::cerr << "- Impossible de récupérer le noeud camera !" << std::endl;
 			return false;
 		}
 
-		_camera = TrackballCamera(
+		// Initialize the camera
+		_Camera = TrackballCamera(
 			cameraDatas.get("distanceToCenter", 5).asFloat(),
 			cameraDatas.get("angle",0).get("x",0).asFloat(),
 			cameraDatas.get("angle",0).get("y",0).asFloat()
@@ -98,14 +96,16 @@ namespace Projet
 
 	void Scene::render() {
 		glEnable(GL_DEPTH_TEST);
-		for (auto mesh = _meshes.begin(); mesh != _meshes.end(); ++mesh) {
+
+		// Render each mesh
+		for (auto mesh = _Meshes.begin(); mesh != _Meshes.end(); ++mesh) {
 			glm::mat4 MVMatrix, ProjMatrix, NormalMatrix, ViewMatrix;
 
 			MVMatrix = glm::translate(glm::mat4(1), (*mesh)->_position);
 			MVMatrix = glm::rotate(MVMatrix, glm::radians((*mesh)->_angle), (*mesh)->_rotAxe);
 			MVMatrix = glm::scale(MVMatrix, (*mesh)->_scale);
 
-			ViewMatrix = _camera.getViewMatrix();
+			ViewMatrix = _Camera.getViewMatrix();
 
 			MVMatrix = ViewMatrix * MVMatrix;
 
@@ -113,12 +113,10 @@ namespace Projet
 
 			NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
 
-			std::cout << "addresse scene" << _program << std::endl;
-
-			_program->setTextureLocation(0);
-			_program->setProjectionMatrix( glm::value_ptr(ProjMatrix * MVMatrix) );
-			_program->setWorldMatrix( glm::value_ptr(MVMatrix) );
-			_program->setNormalMatrix( glm::value_ptr(MVMatrix) );
+			_Program->setTextureLocation(0);
+			_Program->setProjectionMatrix( glm::value_ptr(ProjMatrix * MVMatrix) );
+			_Program->setWorldMatrix( glm::value_ptr(MVMatrix) );
+			_Program->setNormalMatrix( glm::value_ptr(MVMatrix) );
 
 			(*mesh)->render();
 		}
