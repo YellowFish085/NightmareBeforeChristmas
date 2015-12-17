376686d745649c499d291479a0be6c9ba4c0d248
Yellow Fish <theo.costantini@gmail.com>
2015-12-17 22:59:09 +0100

    [Clean] Nettoyage du main

--- GLImac-Template/dev/main.cpp
+++ GLImac-Template/dev/main.cpp
@@ -2,30 +2,13 @@
 
 #include <projet/engine.hpp>
 
-using namespace glimac;
-
 int main(int argc, char** argv) {
-    Projet::AppEngine* eng = new Projet::AppEngine(argv[0]);
-    if (!eng->init("shader.vs.glsl", "shader.fs.glsl")) {
+    Projet::AppEngine* engine = new Projet::AppEngine(argv[0]);
+    if (!engine->init("shader.vs.glsl", "shader.fs.glsl")) {
         return EXIT_FAILURE;
     }
 
-    eng->run();
-
-    // // Application loop:
-    // bool done = false;
-    // while(!done) {
-    //     // Event loop:
-    //     SDL_Event e;
-    //     while(windowManager.pollEvent(e)) {
-    //         if(e.type == SDL_QUIT) {
-    //             done = true; // Leave the loop after this iteration
-    //         }
-    //     }
-
-    //     // Update the display
-    //     windowManager.swapBuffers();
-    // }
+    engine->run();
 
     return EXIT_SUCCESS;
 }
