2fdcbb28168073decfb2068ecb5f964b98848b07
Yellow Fish <theo.costantini@gmail.com>
2015-12-17 23:30:00 +0100

    [Clean] Nettoyage + commentaires de engine.cpp

--- GLImac-Template/projet/src/engine.cpp
+++ GLImac-Template/projet/src/engine.cpp
@@ -15,10 +15,13 @@ namespace Projet
 
 	bool AppEngine::init(const char* vsFilename, const char* fsFilename)
 	{
+		std::cout << std::endl << "*********************************************" << std::endl;
+		std::cout << "Début de l'initialisation de l'application." << std::endl << std::endl;
+
 		// Initialize SDL and Opengl
 		// Exit the app if something went wrong
 		if (!initSdlOpenGl()) {
-			std::cerr << "Erreur lors de l'initialisation de SDL et OpenGL." << std::endl;
+			std::cerr << "ERROR: Erreur lors de l'initialisation de SDL et OpenGL." << std::endl;
 			return false;
 		}
 
@@ -26,20 +29,26 @@ namespace Projet
 		// Exit the app if something went wrong
 		_Program = new ShaderProgram(_ApplicationPath);
 		if (!_Program->init(vsFilename, fsFilename)) {
-			std::cerr << "Erreur lors de l'initialisation du ShaderProgram." << std::endl;
+			std::cerr << "ERROR: Erreur lors de l'initialisation du ShaderProgram." << std::endl;
 			return false;
 		}
 
+		// Initialize the scenes
+		// Exit the app if something went wrong
 		if (!initScenes("scenes/scenesList.json")) {
-			std::cerr << "Erreur lors de l'initialisation des scènes." << std::endl;
+			std::cerr << "ERROR: Erreur lors de l'initialisation des scènes." << std::endl;
+			return false;
 		}
+
+		std::cout << "Fin de l'initialisation de l'application." << std::endl;
+		std::cout << "*********************************************" << std::endl;
 
 		return true;
 	}
 
 	bool AppEngine::initSdlOpenGl()
 	{
-		std::cout << "SDL et OpenGl - initialisation..." << std::endl;
+		std::cout << "====" << std::endl << "SDL et OpenGl - initialisation..." << std::endl;
 
 		glimac::SDLWindowManager* windowManager = new glimac::SDLWindowManager(800, 600, "Nightmare Before Christmas");
 
@@ -65,34 +74,36 @@ namespace Projet
 
 	bool AppEngine::initScenes(const char* filename)
 	{
-		std::cout << "Scenes - initialisation..." << std::endl;
+		std::cout << "====" << std::endl << "Scenes - initialisation..." << std::endl;
 
+		// A json file with the scenes is loaded
 		std::ifstream sceneList((_ApplicationPath->dirPath() + "/../assets/" + filename).c_str(), std::ios::in);
 
 		if (!sceneList) {
-			std::cerr << "Impossible d'ouvrir le fichier contenant la liste des scènes " << (_ApplicationPath->dirPath() + "/../assets/" + filename).c_str() << std::endl;
+			std::cerr << "ERROR: Impossible d'ouvrir le fichier contenant la liste des scènes " << (_ApplicationPath->dirPath() + "/../assets/" + filename).c_str() << std::endl;
 			return false;
 		}
-		else {
-			std::cout << "Liste des scenes bien chargée..." << std::endl;
-		}
+		std::cout << "Liste des scenes ouvert..." << std::endl;
 
+		// Retrieve the root node
 		Json::Value root;
 		Json::Reader reader;
 
 		if (!reader.parse(sceneList, root, false)) {
-      std::cerr  << "Erreur lors de la récupération du json !" << std::endl;
+      std::cerr  << "ERROR: Erreur lors de la récupération du json !" << std::endl;
 			return false;
 		}
 
+		// retrieve the "scenes" node
 		const Json::Value scenes = root["scenes"];
 		if (scenes == 0) {
-			std::cerr << "Impossible de récupérer le noeud scenes !" << std::endl;
+			std::cerr << "ERROR: Impossible de récupérer le noeud scenes !" << std::endl;
 			return false;
 		}
 
+		// Initalize each scene
 		for (int i = 0; i < scenes.size(); i++) {
-			std::cout << scenes[i].get("name", 0).asString() << " initialisation..." << std::endl;
+			std::cout << std::endl << scenes[i].get("name", 0).asString() << " - initialisation..." << std::endl;
 
 			Scene* tmp = new Scene(_ApplicationPath, _Program);
 
@@ -100,18 +111,20 @@ namespace Projet
 				_Scenes.push_back(tmp);
 			}
 			else {
-				std::cerr << "Scene " << scenes[i].get("source", 0) << " n'a pas pu être initialisée." << std::endl;
+				std::cerr << "ERROR: Scene " << scenes[i].get("source", 0) << " n'a pas pu être initialisée." << std::endl;
 				return false;
 			}
 		}
 
-		std::cout << "Scenes - initialisées correctement." << std::endl << std::endl;
+		std::cout << std::endl << "Scenes - initialisées correctement." << std::endl << std::endl;
 
 		return true;
 	}
 
 	void AppEngine::run()
 	{
+		std::cout << std::endl << "App run..." << std::endl;
+
 		bool done = false;
 
 		while(!done) {
@@ -120,6 +133,8 @@ namespace Projet
 			}
 			renderScene();
 		}
+
+		std::cout << std::endl << "App run fin." << std::endl;
 	}
 
 	void AppEngine::renderScene()
@@ -134,6 +149,7 @@ namespace Projet
 
 		while(_WindowManager->pollEvent(e)) {
 			if(e.type == SDL_QUIT) {
+				std::cout << std::endl << "Fermeture d'application reçue..." << std::endl;
 				return true;
 			}
 			else {
