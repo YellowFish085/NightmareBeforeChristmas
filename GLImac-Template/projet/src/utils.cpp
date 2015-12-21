b302e3eb5e5fe20ff255da97468677e2a7f35dd0
Yellow Fish <theo.costantini@gmail.com>
2015-12-21 21:48:35 +0100

    Ajout des get pour les chemins des différents fichiers

--- GLImac-Template/projet/src/utils.cpp
+++ GLImac-Template/projet/src/utils.cpp
@@ -13,4 +13,32 @@ namespace Projet
 		gApplicationPath = new glimac::FilePath(argv);
 	}
 
+	std::string getScenesFilePath(const char* filename)
+	{
+		std::string s;
+		s += gApplicationPath->dirPath().c_str() ;
+		s += RELATIVE_ASSETS_PATH;
+		s += filename;
+		return s;
+	}
+
+	std::string getModelFilePath(const char* filename)
+	{
+		std::string s;
+		s += gApplicationPath->dirPath().c_str() ;
+		s += RELATIVE_ASSETS_PATH;
+		s += RELATIVE_CONTENT_PATH;
+		s += filename;
+		return s;
+	}
+
+	void getShaderFilePath(glimac::FilePath* filePath, const char* filename)
+	{
+		*filePath = gApplicationPath->dirPath() + RELATIVE_SHADERS_PATH + filename;
+	}
+
+	void getTextureFilePath(glimac::FilePath* filePath, const char* filename)
+	{
+		*filePath = gApplicationPath->dirPath() + RELATIVE_ASSETS_PATH + RELATIVE_CONTENT_PATH + filename;
+	}
 }
